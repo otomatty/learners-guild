5edbc98e3e561b2338abf09182ee37357fcbadc4
otomatty <akms0929ama@gmail.com>
2024-09-09 18:39:15 +0900

    fix:テーマセクションの初期表示数と追加表示するの変更

--- src/pages/home/components/LearningThemes/LearningThemes.tsx
+++ src/pages/home/components/LearningThemes/LearningThemes.tsx
@@ -9,12 +9,12 @@ import SectionTitle from "../../../../components/SectionTitle/SectionTitle";
 import { themeData } from "../../../../data/themeData";
 
 const LearningThemes: Component = () => {
-  const [visibleCount, setVisibleCount] = createSignal(6); // 初期表示数を6に設定
+  const [visibleCount, setVisibleCount] = createSignal(10); // 初期表示数を6に設定
 
   const themes = themeData.flatMap((category) => category.themes);
 
   const loadMoreThemes = () => {
-    setVisibleCount(visibleCount() + 6); // クリック時に6つずつ追加表示
+    setVisibleCount(visibleCount() + 10); // クリック時に6つずつ追加表示
   };
 
   return (
