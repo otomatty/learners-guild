d3cff3ff6aea934c3936eeb36d45bcca5b5f3fcd
akimasa-sugai <akms0929ama@gmail.com>
2024-09-09 10:11:05 +0900

    fix: テーマのアイコンを修正

--- src/data/themeData.ts
+++ src/data/themeData.ts
@@ -2,26 +2,31 @@ import {
   FaBrandsSquareJs,
   FaBrandsPython,
   FaBrandsJava,
+  FaSolidCode,
   FaSolidBook,
-  FaChalkboardTeacher,
-  FaChartLine,
-  FaClock,
-  FaCode,
-  FaComments,
-  FaFileAlt,
-  FaGlobe,
-  FaGraduationCap,
-  FaLightbulb,
-  FaLock,
-  FaPaintBrush,
-  FaProjectDiagram,
-  FaRocket,
-  FaSchool,
-  FaUserTie,
-} from "solid-icons/fa";
-import { ImBook } from "solid-icons/im";
-import 
-import { SiRuby, SiCplusplus } from "solid-icons/si";
+  FaSolidPaintbrush,
+  FaSolidGlobe,
+  FaSolidChartLine,
+  FaSolidClock,
+  FaSolidComments,
+  FaSolidGraduationCap,
+  FaSolidLightbulb,
+  FaSolidLock,
+  FaSolidRocket,
+  FaSolidSchool,
+  FaSolidUserTie,
+  FaSolidAtom,
+  FaSolidChalkboardUser,
+  FaSolidComment,
+} from 'solid-icons/fa';
+import { AiFillProject, AiFillAccountBook } from 'solid-icons/ai';
+import { ImBook, ImUsers } from 'solid-icons/im';
+import {
+  BiSolidCameraMovie,
+  BiSolidSlideshow,
+  BiRegularMath,
+} from 'solid-icons/bi';
+import { SiRuby, SiCplusplus } from 'solid-icons/si';
 
 export interface Theme {
   category: string;
@@ -30,70 +35,70 @@ export interface Theme {
 
 export const themeData: Theme[] = [
   {
-    category: "プログラミング",
+    category: 'プログラミング',
     themes: [
-      { title: "JavaScript", icon: FaBrandsSquareJs },
-      { title: "Python", icon: FaBrandsPython },
-      { title: "Ruby", icon: SiRuby },
-      { title: "Java", icon: FaBrandsJava },
-      { title: "C++", icon: SiCplusplus },
-      { title: "その他のプログラミング言語", icon: FaCode },
+      { title: 'JavaScript', icon: FaBrandsSquareJs },
+      { title: 'Python', icon: FaBrandsPython },
+      { title: 'Ruby', icon: SiRuby },
+      { title: 'Java', icon: FaBrandsJava },
+      { title: 'C++', icon: SiCplusplus },
+      { title: 'その他のプログラミング言語', icon: FaSolidCode },
     ],
   },
   {
-    category: "デザイン",
+    category: 'デザイン',
     themes: [
-      { title: "UI/UX", icon: FaPaintBrush },
-      { title: "グラフィックデザイン", icon: FaPaintBrush },
-      { title: "Webデザイン", icon: FaGlobe },
-      { title: "動画編集", icon: FaChalkboardTeacher },
+      { title: 'UI/UX', icon: ImUsers },
+      { title: 'グラフィックデザイン', icon: FaSolidPaintbrush },
+      { title: 'Webデザイン', icon: FaSolidGlobe },
+      { title: '動画編集', icon: BiSolidCameraMovie },
     ],
   },
   {
-    category: "資格試験",
+    category: '資格試験',
     themes: [
-      { title: "TOEIC", icon: FaGraduationCap },
-      { title: "ITパスポート", icon: FaLock },
-      { title: "簿記", icon: FaFileAlt },
-      { title: "公務員試験", icon: FaSchool },
-      { title: "その他の資格試験", icon: FaGraduationCap },
+      { title: 'TOEIC', icon: FaSolidGraduationCap },
+      { title: 'ITパスポート', icon: FaSolidLock },
+      { title: '簿記', icon: AiFillAccountBook },
+      { title: '公務員試験', icon: FaSolidSchool },
+      { title: 'その他の資格試験', icon: FaSolidGraduationCap },
     ],
   },
   {
-    category: "読書",
+    category: '読書',
     themes: [
-      { title: "小説", icon: FaSolidBook },
-      { title: "ビジネス書", icon: FaChartLine },
-      { title: "自己啓発", icon: FaLightbulb },
-      { title: "専門書", icon: ImBook },
+      { title: '小説', icon: FaSolidBook },
+      { title: 'ビジネス書', icon: FaSolidChartLine },
+      { title: '自己啓発', icon: FaSolidLightbulb },
+      { title: '専門書', icon: ImBook },
     ],
   },
   {
-    category: "学生向け",
+    category: '学生向け',
     themes: [
-      { title: "数学", icon: FaChalkboardTeacher },
-      { title: "物理", icon: FaChalkboardTeacher },
-      { title: "化学", icon: FaChalkboardTeacher },
-      { title: "英語", icon: FaComments },
-      { title: "歴史", icon: FaChalkboardTeacher },
+      { title: '数学', icon: BiRegularMath },
+      { title: '物理', icon: FaSolidChalkboardUser },
+      { title: '化学', icon: FaSolidAtom },
+      { title: '英語', icon: FaSolidComment },
+      { title: '歴史', icon: FaSolidChalkboardUser },
     ],
   },
   {
-    category: "ビジネス",
+    category: 'ビジネス',
     themes: [
-      { title: "マーケティング", icon: FaChartLine },
-      { title: "プロジェクト管理", icon: FaProjectDiagram },
-      { title: "起業", icon: FaRocket },
-      { title: "リーダーシップ", icon: FaUserTie },
+      { title: 'マーケティング', icon: FaSolidChartLine },
+      { title: 'プロジェクト管理', icon: AiFillProject },
+      { title: '起業', icon: FaSolidRocket },
+      { title: 'リーダーシップ', icon: FaSolidUserTie },
     ],
   },
   {
-    category: "ライフスキル",
+    category: 'ライフスキル',
     themes: [
-      { title: "タイムマネジメント", icon: FaClock },
-      { title: "コミュニケーション", icon: FaComments },
-      { title: "プレゼンテーション", icon: FaChalkboardTeacher },
-      { title: "クリティカルシンキング", icon: FaLightbulb },
+      { title: 'タイムマネジメント', icon: FaSolidClock },
+      { title: 'コミュニケーション', icon: FaSolidComments },
+      { title: 'プレゼンテーション', icon: BiSolidSlideshow },
+      { title: 'クリティカルシンキング', icon: FaSolidLightbulb },
     ],
   },
 ];
